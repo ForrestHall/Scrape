848d9f3534064a27b0973238b3c82ca4c2d788f3
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-23 23:07:31 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -6,7 +6,7 @@ async function start() {
 	const page = await browser.newPage()
 	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
 	const names = await page.evaluate(() => {
-		return Array.from(document.querySelector(".queryfield td")).map(x => x.textContent)
+		return Array.from(document.querySelectorAll(".queryfield td")).map(x => x.textContent)
 	})
 	await fs.writeFile("names.txt", names.join("\r\n"))
 	await browser.close()
