e44eca357e24021c2739b8df44c4314de2d0799f
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-23 21:57:17 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -5,8 +5,7 @@ async function start() {
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
 	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
-
-	
+	await page.screenshot({path: "safer.png", fullPage: true})	
 	const names = await page.evaluate(() => {
 		return Array.from(document.querySelectorAll("body.p.table.tbody.tr:nth-child(2).td.table.tbody.tr:nth-child(2).td.center:nth-child(3).table.tbody.tr:nth-child(11).td")).map(x => x.textContent)
 	})
