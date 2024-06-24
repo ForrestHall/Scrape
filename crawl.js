3271d8a5860ac2e47d7847801700b0b01d5db23f
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-23 20:52:30 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs/promise')
 
 async function start() {
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
-	await page.goto("https://learnwebcode.github.io/practice-requests/")
-	await page.screenshot({path: "amazing.png"})
+	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
+
+	const names = ['Name', 'PowerUnits']
+	await fs.writeFile("names.txt", names.join("\r\n"))
 	await browser.close()
 }
 
