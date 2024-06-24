288cb0f29a7ea9022013ec85c2b20a9d407b3952
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-23 21:08:37 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -7,6 +7,10 @@ async function start() {
 	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
 
 	const names = ['Name', 'PowerUnits']
+	
+	const names = await page.evaluate(() => {
+		return Array.from(document.querySelector("body > p > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > center:nth-child(3) > table > tbody > tr:nth-child(11) > td")).map(x => x.textContent)
+	})
 	await fs.writeFile("names.txt", names.join("\r\n"))
 	await browser.close()
 }
