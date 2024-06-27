bc9375d0c864eb901bad30d34c8d43030a83e92d
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-26 22:36:14 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -11,11 +11,11 @@ async function start() {
 	const value = await page.evaluate(() => {
 			return Array.from(document.querySelectorAll(".queryfield")).map(x => x.textContent)
 			})
-	const removeValue = ['AUTHORIZED FOR Property  For Licensing and Insurance details  click here.']
-	const cleanValue = value.filter(ele => !removeValue.includes(ele));
-	const result = {};
+	//const removeValue = ['AUTHORIZED FOR Property  For Licensing and Insurance details  click here.']
+	//const cleanValue = value.filter(ele => !removeValue.includes(ele));
+	//const result = {};
 
-	names.forEach((name, idx) => result[name] = cleanValue[idx]);
+	names.forEach((name, idx) => result[name] = value[idx]);
 	//console.log(names)
 	//console.log(value)
 	//console.log(cleanValue)
