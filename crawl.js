484d1c76dc67ed419110c8c6146f6e9b1e9f70e1
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-26 22:20:40 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -7,17 +7,15 @@ async function start() {
 	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
 	const names = await page.evaluate(() => {
 			return Array.from(document.querySelectorAll(".querylabel")).map(x => x.textContent)
-			
 			})
 	const value = await page.evaluate(() => {
 			return Array.from(document.querySelectorAll(".queryfield")).map(x => x.textContent)
-			array.filter( 
-				function(value) {
-  					return value !== 'AUTHORIZED FOR Property  For Licensing and Insurance details  click here.'
-				})
 			})
+	const removeValue = ['AUTHORIZED FOR Property  For Licensing and Insurance details  click here.']
+	const cleanValue = .filter(ele => !removeValues.includes(ele));
 	console.log(names)
 	console.log(value)
+	console.log(cleanValue)
 
 	await fs.writeFile("names.txt", names.join("\r\n"))
 	await browser.close()
