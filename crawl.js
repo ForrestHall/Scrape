ce625d8ea4bfdcc68796bd0c8632d160a2347cbb
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-26 22:13:38 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -6,13 +6,16 @@ async function start() {
 	const page = await browser.newPage()
 	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
 	const names = await page.evaluate(() => {
-		return Array.from(document.querySelectorAll(".querylabel")).map(x => x.textContent)
-		array.filter( function(value) {
-  		return value !== 'AUTHORIZED FOR Property  For Licensing and Insurance details  click here.'})
-	})
+			return Array.from(document.querySelectorAll(".querylabel")).map(x => x.textContent)
+			
+			})
 	const value = await page.evaluate(() => {
-		return Array.from(document.querySelectorAll(".queryfield")).map(x => x.textContent)
-	})
+			return Array.from(document.querySelectorAll(".queryfield")).map(x => x.textContent)
+			array.filter( 
+				function(value) {
+  					return value !== 'AUTHORIZED FOR Property  For Licensing and Insurance details  click here.'
+				})
+			})
 	console.log(names)
 	console.log(value)
 
