f5218eef4356a2db515b48f0639932f29fdfc3aa
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-26 22:54:00 -0400

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -4,8 +4,8 @@ const fs = require('fs/promises')
 async function start() {
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
-/*	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
-	const names = await page.evaluate(() => {
+	await page.goto("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&query_type=queryCarrierSnapshot&query_param=USDOT&query_string=4043629")
+/*	const names = await page.evaluate(() => {
 			return Array.from(document.querySelectorAll(".querylabel")).map(x => x.textContent)
 			})
 	const value = await page.evaluate(() => {
