69c07a58f18918ef0ce059ee650175c33a9e34fe
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-27 22:37:04 -0400

    Update mc.js

--- mc.js
+++ mc.js
@@ -19,9 +19,8 @@ async function run() {
     //console.log(tds)
     const data = await page.evaluate(() => {
       const dataObject = {};
-      const tbody = document.getElementsByTagName('table');
-      //return tbody;
-              console.log(tbody); 
+      const tbody = document.getElementsByTagName('th');
+      return tbody;
 
         /*for (const row of table.rows) {
           if (!row.querySelector('th')) continue; // Skip headers.
