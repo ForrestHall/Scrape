67e385bdf03f22ed33b56c34fa56f06740d196e8
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-27 21:27:25 -0400

    Create mc.js

---
+++ mc.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+async function run() {
+  // usual browser startup:
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // we can use absolute paths like
+    //await page.goto("file://home/ec2-user/EXAMPLE/Scrape/data.html");  
+    
+
+    //  use relative paths: 
+    // below will select test.html that is in the same directory as the script
+    await page.goto(`file:${path.join(__dirname, 'data.html')}`);
+
+    console.log(await page.content());
+    browser.close();
+}
+ 
+run();
