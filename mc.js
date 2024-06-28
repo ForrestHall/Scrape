d245841a5fb31084e30ff74623e93f606d77a076
Forrest Hall <38922530+ForrestHall@users.noreply.github.com>
2024-06-27 23:19:59 -0400

    Update mc.js

--- mc.js
+++ mc.js
@@ -18,7 +18,7 @@ async function run() {
     //return tds.map(td => td.innerText)
     //console.log(tds)
     const data = await page.evaluate(() => {
-    const tds = Array.from(document.querySelector('table:nth-of-type(1) td'))
+    const tds = Array.from(document.querySelector('table:nth-of-type(0) td'))
     return tds.map(td => td.innerText)
     });
 
