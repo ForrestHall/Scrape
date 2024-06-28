5a4c4f0efb1a46b39f3bba6e67fcde39f28225fd
ForrestHall <forrestbrandonhall@gmail.com>
2024-06-28 09:50:06 -0400

    return tds

--- mc.js
+++ mc.js
@@ -20,7 +20,7 @@ async function run() {
 
     const data = await page.evaluate(() => {
     const tds = Array.from(document.querySelectorAll('table tbody tr td'))
-    return tds.map(td => td.innerText)
+    return tds//.map(td => td.innerText)
     });
 
 console.log(data);
