37de10f6c69ccf1d9600f2b94123d3f062c4f840
ForrestHall <forrestbrandonhall@gmail.com>
2024-06-28 10:08:35 -0400

    comment

--- mc.js
+++ mc.js
@@ -21,9 +21,9 @@ async function run() {
 
     const data = await page.evaluate(() => {
     const tds = document.body.innerText//Array.from(document.querySelectorAll('table tbody tr td'))
-    const numb = tds.search(pattern);
+    //const numb = tds.search(pattern);
 
-    return numb;
+    return tds;
 
     });
 
